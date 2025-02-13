8d13622884ddb2b36d1a81dd699c8a4cad1604d6
PawelHarasiuk <pharasiuk01@gmail.com>
2025-02-13 15:02:53 +0100

    created detecting gestures

---
+++ cv/gesture/main.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+
+base_options = python.BaseOptions(model_asset_path='cv/gesture/gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
+cap = cv2.VideoCapture(1)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+    result_gesture = recognizer.recognize(mp_image)
+    if result_gesture.gestures:
+        gesture_name = result_gesture.gestures[0][0].category_name
+        cv2.putText(frame, f"Gesture: {gesture_name}", (50, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    cv2.imshow('Gesture Recognition', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
