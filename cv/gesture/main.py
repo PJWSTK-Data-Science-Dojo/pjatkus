ca2e0d1193fe694e8bf2f1fa01bab889d2636e88
PawelHarasiuk <pharasiuk01@gmail.com>
2025-02-13 15:59:22 +0100

    gestures: added support for two hands

--- cv/gesture/main.py
+++ cv/gesture/main.py
@@ -3,14 +3,13 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+base_options = python.BaseOptions(model_asset_path='cv/gesture/gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options, num_hands=2)
+recognizer = vision.GestureRecognizer.create_from_options(options)
 
 # mp_hands = mp.solutions.hands
 # mp_drawing = mp.solutions.drawing_utils
 # hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-
-base_options = python.BaseOptions(model_asset_path='cv/gesture/gesture_recognizer.task')
-options = vision.GestureRecognizerOptions(base_options=base_options)
-recognizer = vision.GestureRecognizer.create_from_options(options)
 
 cap = cv2.VideoCapture(1)
 
@@ -28,10 +27,26 @@ while cap.isOpened():
 
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
     result_gesture = recognizer.recognize(mp_image)
-    if result_gesture.gestures:
-        gesture_name = result_gesture.gestures[0][0].category_name
-        cv2.putText(frame, f"Gesture: {gesture_name}", (50, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    left_hand_gesture = "Unknown"
+    right_hand_gesture = "Unknown"
+    seen_left = False
+    seen_right = False
+
+    if result_gesture.gestures and result_gesture.handedness:
+        for i in range(len(result_gesture.gestures)):
+            for j in range(len(result_gesture.handedness[i])):
+                hand_label = result_gesture.handedness[i][j].category_name
+                gesture_name = result_gesture.gestures[i][j].category_name
+                if hand_label == "Left" and not seen_left:
+                    left_hand_gesture = gesture_name
+                elif hand_label == "Right" and not seen_right:
+                    right_hand_gesture = gesture_name
+
+                if seen_right and seen_left:
+                    break
+    cv2.putText(frame, f"Left: {left_hand_gesture} | Right: {right_hand_gesture}",
+                (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Gesture Recognition', frame)
 
